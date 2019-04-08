cfbb5128d6346a116718e29161450165600ef32a
Eachin <EachinChung@gmail.com>
2019-04-09 01:54:52 +0800

    修复了一些Bug...

--- main.py
+++ main.py
@@ -9,47 +9,36 @@ import requests
 class ElectricityThread(threading.Thread):
 
     def __init__(self, data):
-
         threading.Thread.__init__(self)
         self.data = data
         self.electricity = {}
 
     def run(self):
-
         for room_name in self.data:
             button = True
             while button:
                 button = self.post(room_name, self.data[room_name])
 
     def post(self, room_name, room_id):
-
         url = 'http://axf.nfu.edu.cn/electric/getData/getReserveAM'
         data = {'roomId': room_id}
 
         try:
             session = requests.session()
             response = session.post(url, data=data, timeout=1)
-
         except OSError:
             return True
-
         else:
-
             try:
                 electric_quantity = json.loads(response.text)
                 electric_quantity = electric_quantity['data']['remainPower']
-
             except TypeError:
                 self.electricity.update({room_name: 'NULL'})
                 return False
-
             else:
-
                 if float(electric_quantity) < 0:
                     electric_quantity = '0'
-
                 else:
-
                     electric_quantity = round(float(electric_quantity), 2)
                     electric_quantity = str(int(float(electric_quantity) * 100))
 
